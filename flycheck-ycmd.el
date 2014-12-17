2f723d765c322705e1ff9192e8698ea878426b71
Austin Bingham <austin.bingham@gmail.com>
2014-12-17 14:23:12 +0100

    Filled out header docs for flycheck-ycmd.el.

--- flycheck-ycmd.el
+++ flycheck-ycmd.el
@@ -1,5 +1,49 @@
 ;;; flycheck-ycmd -- flycheck integration for ycmd
+;; Copyright (c) 2014 Austin Bingham
+;;
+;; Author: Austin Bingham <austin.bingham@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/abingham/emacs-ycmd
+;; Package-Requires: ((emacs "24") (flycheck "0.22"))
+;;
+;; This file is not part of GNU Emacs.
+;;
 ;;; Commentary:
+;;
+;; Description:
+;;
+;; This provides flycheck integration for ycmd. It allows flycheck to
+;; use ycmd's parse results for it display. It essentially works by
+;; caching the ycmd parse results and then using them when the checker
+;; is invoked.
+;;
+;; Basic usage:
+;;
+;;  (require 'flycheck-ycmd)
+;;  (flycheck-ycmd-setup)
+;;
+;;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
 ;;; Code:
 
 (require 'flycheck)
