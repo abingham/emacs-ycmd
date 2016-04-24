010769a1cfe0806f8cc54a963b610f8d996368ba
ptrv <mail@petervasil.net>
2016-04-24 11:06:36 +0200

    Update package requires

--- ycmd.el
+++ ycmd.el
@@ -6,7 +6,7 @@
 ;;          Peter Vasil <mail@petervasil.net>
 ;; Version: 0.9.1
 ;; URL: https://github.com/abingham/emacs-ycmd
-;; Package-Requires: ((emacs "24") (f "0.17.1") (dash "1.2.0") (deferred "0.3.2") (popup "0.5.0") (cl-lib "0.5") (let-alist "1.0.4"))
+;; Package-Requires: ((emacs "24.3") (f "0.17.1") (dash "2.12.0") (deferred "0.3.2") (popup "0.5.0") (cl-lib "0.5") (let-alist "1.0.4"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
