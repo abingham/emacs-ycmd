0662a01605b14bb1070c7d6274c990b736af51da
ptrv <mail@petervasil.net>
2016-08-15 22:01:32 +0200

    Add support for typescript

---
+++ test/resources/test.ts
@@ -0,0 +1,36 @@
+class Foo {
+  // Unicode string: 说话
+  methodA() {}
+  methodB() {}
+  methodC(
+    a: {
+      foo: string;
+      bar: number;
+    }
+  ) {}
+}
+
+var foo = new Foo();
+
+// line 12, column 6
+foo.m
+
+
+/**
+ * Class documentation
+ *
+ * Multi-line
+ */
+class Bar {
+
+  /**
+   * Method documentation
+   */
+  testMethod() {}
+}
+
+var bar = new Bar();
+bar.testMethod();
+bar.nonExistingMethod();
+
+Bar.apply()
