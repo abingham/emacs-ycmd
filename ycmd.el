ca51cbce87f671f2bb133d1df9f327bb8f1bb729
ptrv <mail@petervasil.net>
2016-12-22 19:39:13 +0100

    Another spelling fix

--- ycmd.el
+++ ycmd.el
@@ -247,7 +247,7 @@ use `ycmd-parse-buffer'."
 
 (defcustom ycmd-auto-trigger-semantic-completion t
   "If non-nil, semantic completion is turned off.
-Semanic completion is still available if
+Semantic completion is still available if
 `ycmd-force-semantic-completion' is non-nil."
   :type 'boolean)
 
